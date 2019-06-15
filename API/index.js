a9567b623a2864aed88cc830dfc9ff8f8d79b049
Karen Aguilar <36086237+hix-karen-aguilar@users.noreply.github.com>
2019-06-15 14:47:16 -0500

    commit de checkpoint

--- API/index.js
+++ API/index.js
@@ -3,10 +3,10 @@ const bodyparser = require('body-parser');
 const mongoose = require('mongoose');
 const Ctrl = require('./controllers/pacienteCRUD.js');
 const { Paciente } = require('./models/Paciente');
-//const cors = require('cors');
+const cors = require('cors');
 
 const app = express();
-//app.use(cors());
+app.use(cors());
 app.use(bodyparser.urlencoded({ extended : true}));
 app.use(bodyparser.json());
 
@@ -14,8 +14,9 @@ app.use(bodyparser.json());
 //CONTRASEÑA
 const PORT = process.env.PORT || 3001;
 
+const URL_MONGO = `mongodb+srv://karen:151111034@cluster0-offbi.mongodb.net/test?retryWrites=true&w=majority`;
 //const URL_MONGO = `mongodb+srv://${DB_USER}:${DB_PASS}@cluster0-ijbr8.mongodb.net/test?retryWrites=true`;
-const URL_MONGO =  'mongodb+srv://karen:ABZWO1RKXRt6A2K4@laboratorio-pdxyp.mongodb.net/test?retryWrites=true&w=majority';
+//const URL_MONGO =  'mongodb+srv://karen:ABZWO1RKXRt6A2K4@laboratorio-pdxyp.mongodb.net/test?retryWrites=true&w=majority';
 
 console.log("LOG: ", URL_MONGO);
 
@@ -44,10 +45,18 @@ app.get('/pacientes', (req, res) => {
     })
 });
 
-app.get('/pacientes', (req, res) => {
-    const objPaciente = req.body;
-    //
-})
+//POST
+app.post('pacientes', (req, res) => {
+    console.log("Entré a pos :) ");
+    Paciente(req.body).save((err, paciente) => {
+        err ? res.status(400).send({
+            message: 'Checar petición',
+            errorMongo: err
+        }) : res.status(201).send(paciente);
+        console.log("SAliendo de post");
+        
+    });
+});
 
 
 app.listen(PORT, () => {
