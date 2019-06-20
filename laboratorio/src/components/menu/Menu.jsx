8f746e527aa2a16b2a36ec9c310dffcc8becb152
git-pusher <rachel.palomeque@gmail.com>
2019-06-20 11:09:39 -0500

    Estilos en página de inicio (sesión)

--- laboratorio/src/components/menu/Menu.jsx
+++ laboratorio/src/components/menu/Menu.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 
-import logo from '../../../public/img/logo.png';
+import labo from '../../../public/img/logo.png';
 import Login from '../sesion/Login';
 // import Contacto from '../../pages/Contacto'
 import './menu.css';
@@ -18,7 +18,7 @@ class Menu extends Component {
 			<nav className="navbar fixed-top navbar-expand-lg navbar-light" >
 				<div className="container-fluid">
 				<NavLink className="navbar-brand" to="/">
-					<img src={logo} alt="Landsteiner Scientific" height="50px"/>
+					<img src={labo} alt="Landsteiner Scientific" height="50px"/>
 				</NavLink>
 					<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
 						<span className="navbar-toggler-icon"></span>
