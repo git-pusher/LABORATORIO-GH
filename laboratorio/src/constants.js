569fa72df085ac7beb4144b3c9d99aacd5def1c2
git-pusher <rachel.palomeque@gmail.com>
2019-07-10 23:25:31 -0500

    configuración de package.json en raiz

--- laboratorio/src/constants.js
+++ laboratorio/src/constants.js
@@ -1,6 +1,6 @@
-//  const API_URL = 'http://localhost:3001/';
-const DB_USER = process.env.DB_USER;
-const DB_PASS = process.env.DB_PASS;
-const API_URL = 'https://grace-hooper.herokuapp.com/';
+ const API_URL = 'http://localhost:3001/';
+// const DB_USER = process.env.DB_USER;
+// const DB_PASS = process.env.DB_PASS;
+// const API_URL = 'https://grace-hooper.herokuapp.com/';
 // SECRET_TOKEN: 'claveToken';
 export default API_URL;
