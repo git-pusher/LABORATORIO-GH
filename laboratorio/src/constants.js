6476ce5384b7bcb6bcc62cc88d5e963e0835b352
git-pusher <rachel.palomeque@gmail.com>
2019-07-11 00:53:01 -0500

    probndo otro cambo

--- laboratorio/src/constants.js
+++ laboratorio/src/constants.js
@@ -1,6 +1,6 @@
-//  const API_URL = 'http://localhost:3001/';
+ const API_URL = 'http://localhost:3001/';
 // const DB_USER = process.env.DB_USER;
 // const DB_PASS = process.env.DB_PASS;
-const API_URL = 'https://laboratorio-gh.herokuapp.com/';
+// const API_URL = 'https://laboratorio-gh.herokuapp.com/';
 // SECRET_TOKEN: 'claveToken';
 export default API_URL;
